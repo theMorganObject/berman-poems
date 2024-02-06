00ae71c95099c4965252008168850a95b37ee887
Morgan OShaughnessey <123529383+theMorganObject@users.noreply.github.com>
2024-02-06 07:06:31 -1000

    next/prev buttons on poem stepper

--- src/app/poems/[slug]/page.tsx
+++ src/app/poems/[slug]/page.tsx
@@ -1,11 +1,20 @@
+import { getSortedPoemSlugs } from '../../../../lib/poems-util';
+
 import PoemStepper from '@/components/poems/poem-stepper/PoemStepper';
 
-export default function Page({ params }: { params: { slug: string } }) {
+async function getPoemSlugs() {
+  const res = await getSortedPoemSlugs();
+  const allPoemSlugs = await res;
+  return allPoemSlugs;
+}
+
+export default async function Page({ params }: { params: { slug: string } }) {
   const poemID = params.slug;
+  const allSLugs = await getPoemSlugs();
 
   return (
     <section className='relative flex flex-col items-center max-h-screen overflow-y-scroll p-2 pb-16'>
-      <PoemStepper poemId={`${poemID}`} />
+      <PoemStepper poemId={`${poemID}`} slugsArr={allSLugs} />
     </section>
   );
 }
