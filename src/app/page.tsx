e0d97f6c0ad4c7bcfd36a3f31f5d55e504b44d21
Morgan OShaughnessey <123529383+theMorganObject@users.noreply.github.com>
2024-07-17 17:47:43 +0100

    homepage layout styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
-    <main className='relative flex flex-col max-h-screen items-center p-2'>
+    // TODO: remove flex box
+    <main className='relative flex flex-col items-center h-screen p-2'>
       <Hero />
     </main>
   );
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,38 +3,38 @@ import { GiQuillInk } from 'react-icons/gi';
 
 function Hero() {
   return (
-    <>
-      <h1 className='text-6xl text-center font-tangerine pt-12 mb-2 xs:text-7xl'>
+    <div className='flex flex-col justify-around items-center h-full p-4'>
+      <h1 className='text-6xl text-center font-tangerine pt-4 xs:text-7xl'>
         Collected Poems
       </h1>
-      <p className='text-2xl font-tangerine mb-12 xs:text-3xl'>
+      <p className='text-2xl font-tangerine mt-4 xs:text-3xl'>
         Stephen Harlow Berman
       </p>
-      <div className='text-6xl text-gray-9/80 ml-2 mb-16 xs:text-7xl'>
+      <div className='text-6xl text-gray-9/80 mt-6 xs:text-7xl'>
         <GiQuillInk />
       </div>
-      <p className='max-w-72 text-center mb-24 italic xs:text-lg'>
+      <p className='max-w-72 text-center mt-6 italic xs:text-lg'>
         A living and ongoing collection
         <br />
-        Over 700 poems, one per day
+        Over 7 poems, one per day
         <br />
         since August 21st, 2020.
       </p>
-      <div className='flex flex-col gap-6'>
+      <div className='flex flex-col gap-4 mt-8 w-full max-w-md'>
         <Link
           href='/poems/poem-0001'
-          className='bg-transparent text-center border-2 border-gray-8 text-black hover:bg-gray-8 hover:text-gray-0 active:bg-gray-9 active:text-gray-1 px-2 py-1 transition duration-200 ease-in xs:text-lg'
+          className='bg-transparent text-center border-2 border-gray-8 text-black hover:bg-gray-8 hover:text-gray-0 active:bg-gray-9 active:text-gray-1 px-4 py-2 transition duration-200 ease-in xs:text-lg'
         >
-          <button>Read Today&apos;s Poem</button>
+          Read Today&apos;s Poem
         </Link>
         <Link
           href='/poems'
-          className='bg-transparent text-center border-2 border-gray-8 text-black hover:bg-gray-8 hover:text-gray-0 active:bg-gray-8 active:text-gray-1 px-2 py-1 transition duration-200 ease-in xs:text-lg'
+          className='bg-transparent text-center border-2 border-gray-8 text-black hover:bg-gray-8 hover:text-gray-0 active:bg-gray-8 active:text-gray-1 px-4 py-2 transition duration-200 ease-in xs:text-lg'
         >
-          <button>Browse All Poems</button>
+          Browse All Poems
         </Link>
       </div>
-    </>
+    </div>
   );
 }
 
