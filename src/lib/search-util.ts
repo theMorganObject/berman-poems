68c76d1cdc10795b2b0ab84bb8f058ed5fbb98b1
Morgan OShaughnessey <123529383+theMorganObject@users.noreply.github.com>
2024-12-17 12:49:35 -1000

    migrated search by number to db

--- src/lib/search-util.ts
+++ src/lib/search-util.ts
@@ -1,14 +1,3 @@
-const numbersWithMultiplePoems = [
-  '6',
-  '44',
-  '90',
-  '94',
-  '241',
-  '718',
-  '726',
-  '735',
-];
-
 export const keypadButtons: string[] = [
   '1',
   '2',
@@ -23,18 +12,3 @@ export const keypadButtons: string[] = [
   '0',
   'Delete',
 ];
-
-export const getPoemUrlString = (poemNumber: string): string => {
-  let suffix = '';
-
-  if (numbersWithMultiplePoems.includes(poemNumber)) {
-    suffix = 'a';
-  }
-
-  return poemNumber.padStart(4, '0') + suffix;
-};
-
-export const isValidPoemNumber = (poemNumber: string): boolean => {
-  const number = parseInt(poemNumber, 10);
-  return number >= 1 && number <= 754;
-};
